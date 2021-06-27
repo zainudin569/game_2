40568f11d548bb919a1a28eb5ab3f020047a0480
zainudin569 <81910959+zainudin569@users.noreply.github.com>
2021-06-27 10:57:02 +0300

    C:\Users\zainu\Desktop\курс\game_2\Ozhaev Zaynudin Game_2.cpp -o C:\Users\zainu\Desktop\курс\game_2\Ozhaev Zaynudin Game_2.exe  -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wmissing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -D_DEBUG -D_EJUDGE_CLIENT_SIDE -D_EJC

--- Ozhaev Zaynudin Game_2.cpp
+++ Ozhaev Zaynudin Game_2.cpp
@@ -39,7 +39,7 @@ struct Ball
     COLORREF Color;
     COLORREF FillColor;
 
-    void Physics (int* score1, int* score2, int dt);
+    void Physics (int* score1, int* score2, int ax, int ay, int dt);
     void Drow();
     };
 
@@ -175,7 +175,7 @@ void Key::Control (Ball* ball, int* F4_Col)
 
 //---------------------------------------------------------------------------------
 
-void Ball::Physics (int* score1, int* score2, int dt)
+void Ball::Physics (int* score1, int* score2, int ax, int ay, int dt)
     {
     int W_org   = W;
     int W_org_2 = 0;
